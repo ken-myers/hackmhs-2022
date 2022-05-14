ca3f1685ca0b94553f156800a8df4e81d2f92998
Michelle Nguyen <michellehwin@gmail.com>
2022-05-14 18:30:48 -0500

    add team component

--- hackmhs-2022/src/app/app-routing.module.ts
+++ hackmhs-2022/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { TeamComponent } from './components/team/team.component';
 
 
-const routes: Routes = [{path: 'login', component: LoginComponent}, {path:'task-list', component: TaskListComponent}];
+const routes: Routes = [{ path: 'login', component: LoginComponent },
+{ path: 'task-list', component: TaskListComponent },
+{ path: 'team', component: TeamComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
