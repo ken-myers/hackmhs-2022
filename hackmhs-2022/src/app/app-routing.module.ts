b357c16f5ea5872426754b5ebc8fd390abf4caca
Ken Myers <kennmyersiii@gmail.com>
2022-05-14 22:03:18 -0500

    add team

--- hackmhs-2022/src/app/app-routing.module.ts
+++ hackmhs-2022/src/app/app-routing.module.ts
@@ -7,9 +7,10 @@ import { TeamComponent } from './components/team/team.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
 
 
-const routes: Routes = [{path: 'login', component: LoginComponent}, {path:'task-list', component: TaskListComponent},
-  {path: 'home', component: HomeComponent}
-];
+const routes: Routes = [{ path: 'login', component: LoginComponent },
+{ path: 'task-list', component: TaskListComponent },
+{ path: 'team', component: TeamComponent },
+{ path: 'timeline', component: TimelineComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
